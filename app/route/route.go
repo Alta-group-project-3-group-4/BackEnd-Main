85de8e1ceabe9b61812fda0ed2671ce491c8068a
Rischiyuda <rischiyuda17@gmail.com>
2023-03-14 08:50:30 +0700

    touch main

---
+++ app/route/route.go
@@ -0,0 +1,29 @@
+package router
+
+import (
+	middlewares "airbnb/app/middleware"
+	"airbnb/utils/helpers"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
+func InitRouter(db *gorm.DB, e *echo.Echo) {
+	middlewares.Cors(e)
+	middlewares.BasicLogger(e)
+	e.GET("/", index)
+	SwaggerRouter(e)
+	AuthRouter(db, e)
+	UserRouter(db, e)
+
+}
+
+func index(c echo.Context) error {
+	var data = map[string]interface{}{
+		"message":       "Welcome to API airbnb Project",
+		"documentation": "/swagger/index.html",
+	}
+
+	return c.JSON(http.StatusOK, helpers.ResponseSuccess("-", data))
+}
--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"airbnb/app/config"
+	"airbnb/app/database"
+	router "airbnb/app/route"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	// docs.InitSwagger()
+	cfg := config.InitConfig()
+	db := database.InitDBMysql(*cfg)
+	database.InitMigration(db)
+
+	e := echo.New()
+	router.InitRouter(db, e)
+	e.Logger.Fatal(e.Start(":8080"))
+}
